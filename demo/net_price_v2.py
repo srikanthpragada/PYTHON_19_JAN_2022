4ef793d651e39bf0a737df22b858d0863a9ca4a4
srikanthpragada <srikanthpragada@gmail.com>
2022-01-22 19:54:33 +0530

    22-JAN

--- demo/net_price_v2.py
+++ demo/net_price_v2.py
@@ -1,9 +1,14 @@
 # Calculate net price
-
 data = input("Enter price :")
-price = int(data)     # convert string to int
-discount = price * 10 / 100
+price = int(data)  # convert string to int
+
+if price > 5000:
+    discount = price * 20 / 100
+else:
+    discount = price * 10 / 100
+
 net_price = price - discount
+
 print(f"Price        {price:8.2f}")
 print(f"- Discount   {discount:8.2f}")
 print(f"Net price    {net_price:8.2f}")
